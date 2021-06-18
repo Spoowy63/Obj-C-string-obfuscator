33421518e78d47238ab4cc732619fca2494335fb
Spoowy <78769981+Spoowy63@users.noreply.github.com>
2021-06-18 11:39:20 +0200

    Update Obf.py

--- Obf.py
+++ Obf.py
@@ -30,5 +30,6 @@ finished_obf_string = filler.join(letters_list)
 
 os.system('clear||cls')
 print('\nCopy the code below for your project:\n')
+
 print("//Made by: github.com/Spoowy63")
 print(f"NSString *{variable_name}; \n{variable_name} = [@\"{finished_obf_string}\" stringByReplacingOccurrencesOfString:@\"{filler}\" withString:@\"\"];")
